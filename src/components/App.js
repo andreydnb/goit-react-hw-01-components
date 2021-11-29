b4ec0b76756da05c08e61534a313d79e15d41338
andreydnb <a.gritsay2@gmail.com>
2021-11-30 00:14:53 +0200

    add task2

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics'
 import user from './Profile/user.json';
+import data from './Statistics/data.json'
 
 
 const App = () => (
   <>
     <Profile
-      name={user.name}
+      name={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
     />
- 
+     <Statistics title="Upload stats" stats={data} />
+  
   </>
 );
 
