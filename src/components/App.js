51199b796a067a2f7d8a74271f4c94f3ac450213
andreydnb <a.gritsay2@gmail.com>
2021-11-30 13:14:03 +0200

    add task 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics'
+import FriendsList from './FriendList/FriendList';
 import user from './Profile/user.json';
 import data from './Statistics/data.json'
+import friends from './FriendList/friends.json'
 
 
 const App = () => (
@@ -14,7 +16,8 @@ const App = () => (
       avatar={user.avatar}
       stats={user.stats}
     />
-     <Statistics title="Upload stats" stats={data} />
+    <Statistics title="Upload stats" stats={data} />
+    <FriendsList friends={friends} />
   
   </>
 );
